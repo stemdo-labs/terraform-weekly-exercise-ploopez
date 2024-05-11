ad855de118317003603ebfd1af403d58f0dbb481
ploopez <166410013+ploopez@users.noreply.github.com>
2024-05-11 11:51:00 +0200

    worload funcionando

---
+++ soluciones/workload/main.tf
@@ -0,0 +1,41 @@
+terraform {   
+    required_providers {     
+        azurerm = {       
+            source = "hashicorp/azurerm"       
+            version = "3.102.0"     
+            
+        }   
+    }   
+    # backend "azurerm" {       
+    #     resource_group_name  = "rg1plopez-lab01"       
+    #     storage_account_name = "sta1plopez"       
+    #     container_name       = "tfstate"       
+    #     key                  = "terraform.tfstate"   
+    # } 
+}  
+
+provider "azurerm" {   
+    features {} 
+}
+
+module "terraform_azure_workload" {   
+    source = "github.com/stemdo-labs/terraform-exercises-ploopez/soluciones/modulo-weekly-exercise" 
+    resource_group_name = var.resource_group_name     
+    location = var.location
+    vnet_name = var.vnet_name
+    vnet_address_space = var.vnet_address_space
+    subnets = var.subnets
+    network_interface = var.network_interface
+    linux_virtual_machine = var.linux_virtual_machine
+    load_balancer = var.load_balancer
+
+    
+}
+
+
+resource "azurerm_storage_container" "container" {
+  name                  = "tfstate"
+  storage_account_name  = var.storage_account_name
+  container_access_type = "private"
+}
+
