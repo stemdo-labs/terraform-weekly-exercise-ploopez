64c686cb30d62d8762414c63f3e10a65e5d21ffa
ploopez <166410013+ploopez@users.noreply.github.com>
2024-05-12 19:54:47 +0200

    Update main.tf IMPORTANTE
    arreglo del storage container y el tfstate

--- soluciones/workload/main.tf
+++ soluciones/workload/main.tf
@@ -1,25 +1,19 @@
-terraform {   
-    required_version = ">= 0.13"
-    required_providers {     
-        azurerm = {       
-            source = "hashicorp/azurerm"       
-            version = "3.102.0"     
-
-        }   
-    }   
-    #backend "azurerm" {       
-    #     resource_group_name  = "rg1plopez-lab01"       
-    #     storage_account_name = "sta1plopez"       
-    #     container_name       = "tfstate"       
-    #     key                  = "terraform.tfstate"   
-    #} 
-}  
-
+terraform {
+  required_version = ">= 1.5.7"
+  backend "azurerm" {
+    resource_group_name  = "rg1plopez-lab01"
+    storage_account_name = "sta1plopez"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+ 
 provider "azurerm" {
   features {}
 }
 
 
+data "azurerm_client_config" "current" {}
 module "terraform_azure_workload" {   
     source = "github.com/stemdo-labs/terraform-exercises-ploopez/soluciones/modulo-weekly-exercise" 
     resource_group_name = var.resource_group_name     
@@ -31,13 +25,6 @@ module "terraform_azure_workload" {
     linux_virtual_machine = var.linux_virtual_machine
     load_balancer = var.load_balancer
 
-    
 }
 
-
-#resource "azurerm_storage_container" "container" {
-#  name                  = "tfstate"
-#  storage_account_name  = var.storage_account_name
-#  container_access_type = "private"
-#}
 
